ef2ee29746d75ed398c19749c56af59e7c2382cb
Sam Orozco <sam.orozco@apptreesoftware.com>
2018-11-28 05:52:55 -0800

    Routing the schedule detail successfully

--- load_tester_frontend/lib/src/route_paths.dart
+++ load_tester_frontend/lib/src/route_paths.dart
@@ -3,5 +3,5 @@ import 'package:angular_router/angular_router.dart';
 class RoutePaths {
   static final baseRoute = RoutePath(path: '');
   static final createSchedule = RoutePath(path: 'schedule/create');
-  static final scheduleDetail = RoutePath(path: 'schedule');
+  static final scheduleDetail = RoutePath(path: 'schedule/:scheduleId');
 }
