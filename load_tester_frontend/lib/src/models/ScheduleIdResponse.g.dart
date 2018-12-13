eef95dadece142d0942ecd10c1df2a4954b67d2d
Sam Orozco <sam.orozco@apptreesoftware.com>
2018-12-13 14:31:43 -0800

    Adding generated files

---
+++ load_tester_frontend/lib/src/models/ScheduleIdResponse.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ScheduleIdResponse.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ScheduleIdResponse _$ScheduleIdResponseFromJson(Map<String, dynamic> json) {
+  return ScheduleIdResponse(scheduleId: json['schedule_id'] as String);
+}
+
+Map<String, dynamic> _$ScheduleIdResponseToJson(ScheduleIdResponse instance) =>
+    <String, dynamic>{'schedule_id': instance.scheduleId};
