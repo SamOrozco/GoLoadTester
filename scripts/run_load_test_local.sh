f8ee79c98734fc0225191fef4a75e2df1b852c47
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-02-09 12:11:40 -0800

    Run locally script is working

--- scripts/run_load_test_local.sh
+++ scripts/run_load_test_local.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-export dockerFileLocation=../load_tester_backend/docker-files/Dockerfile
+
+# where do we want to execute this script from and go to it
+export location=..
+cd $location
+
+# <location> relative docker file location
+export dockerFileLocation=load_tester_backend/docker-files/Dockerfile
+#desire image name
 export imageName=local_load_tester
+# copy built web relative <location>
 cp -r load_tester_frontend/build load_tester_backend/built-web
+
 docker build -t $imageName -f $dockerFileLocation .
+
 docker run -p 9000:9000 $imageName
